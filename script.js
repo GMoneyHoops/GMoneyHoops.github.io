76a44f5f44f5e5dea9a182e84f7c405cffb8d2ff
GMoneyHoops <112903459+GMoneyHoops@users.noreply.github.com>
2022-12-06 12:36:46 -0500

    6 cards done

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ $(document).ready(function() {
     else {
       alert("try again");
       ClearText();
+      $("#status").html("Try again");
     }
   });
 
@@ -32,16 +33,44 @@ $(document).ready(function() {
     var card1 = {
       card: 1,
       picture : "APP.png",
-      answertext : "Bugs Bunny"
+      answertext : "`"
     };
     
     var card2 = {
       card: 2,
       picture : "Reset.png",
-      answertext : "Bart"
+      answertext : "`"
     };
+        
+    var card3 = {
+      card: 3,
+      picture : "Left_possession.png",
+      answertext : "1"
+    };
+    var card4 = {
+      card: 4,
+      picture : "Right_Possession.png",
+      answertext : "2"
+    };
+    
+    var card5 = {
+      card: 5,
+      picture : "Book_it.png",
+      answertext : "b"
+    };
+        
+    var card6 = {
+      card: 6,
+      picture : "Contact_loop.png",
+      answertext : "numpad 2"
+    };
+
     deck.push(card1);
     deck.push(card2);
+    deck.push(card3);
+    deck.push(card4);
+    deck.push(card5);
+    deck.push(card6);
   }
 
   function Draw() {
