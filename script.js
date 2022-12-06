cb31aa612fd736132ec08d83836623ff83758152
GMoneyHoops <112903459+GMoneyHoops@users.noreply.github.com>
2022-12-06 11:30:40 -0500

    updating cards

--- script.js
+++ script.js
@@ -74,6 +74,30 @@ $(document).ready(function() {
     deck.push(card2);
     deck.push(card3);
     deck.push(card4);
+    deck.push(card5);
+    deck.push(card6);
+    deck.push(card7);
+    deck.push(card8);
+    deck.push(card9);
+    deck.push(card10);
+    deck.push(card11);
+    deck.push(card12);
+    deck.push(card13);
+    deck.push(card14);
+    deck.push(card15);
+    deck.push(card16);
+    deck.push(card17);
+    deck.push(card18);
+    deck.push(card19);
+    deck.push(card20);
+    deck.push(card21);
+    deck.push(card22);
+    deck.push(card23);
+    deck.push(card24);
+    deck.push(card25);
+    deck.push(card26);
+    deck.push(card27);
+    deck.push(card28);
   }
 
   function Draw() {
