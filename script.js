2a1da8e7639b60ba0e25ed6eb660351512111e23
GMoneyHoops <112903459+GMoneyHoops@users.noreply.github.com>
2022-12-06 08:45:21 -0500

    stlying

--- script.js
+++ script.js
@@ -43,38 +43,12 @@ $(document).ready(function() {
     timer = setInterval(updateTimer, 1000);
   }
 
-  function startGame() {
- 
-    resetValues();
-    updateQuote();
-   
-    // clear old and start a new timer
-    clearInterval(timer);
-    timer = setInterval(updateTimer, 1000);
-  }
-   
-  function resetValues() {
-    timeLeft = TIME_LIMIT;
-    timeElapsed = 0;
-    errors = 0;
-    total_errors = 0;
-    accuracy = 0;
-    characterTyped = 0;
-    quoteNo = 0;
-    input_area.disabled = false;
-   
-    input_area.value = "";
-    quote_text.textContent = 'Click on the area below to start the game.';
-    accuracy_text.textContent = 100;
-    timer_text.textContent = timeLeft + 's';
-    error_text.textContent = 0;
-    restart_btn.style.display = "none";
 
   function Init() {
     var card1 = {
       card: 1,
       picture : "APP.png",
-      answertext : "Bugs Bunny"
+      answertext : "`"
     };
     
     var card2 = {
